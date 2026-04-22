c2f8b6c5fa136db235f39bb9b4beda31246c441c
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-04-22 18:49:57 +0700

    style: frame brackets and add email link

--- src/data/config.ts
+++ src/data/config.ts
@@ -72,6 +72,13 @@ export const siteConfig: SiteConfig = {
                         </svg>`,
     },
     {
+      name: "Email",
+      url: "mailto:nan@nanoo.cloud",
+      icon: `<svg viewBox="0 0 24 24">
+                        <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+                    </svg>`,
+    },
+    {
       name: "My Portfolio",
       url: "https://www.aadnanmt.web.id",
       icon: `<svg viewBox="0 0 24 24">
